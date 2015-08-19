028fdc222ec11d6c5cfb817a9e57470637f86798
Bryan Powell <bryan@metabahn.com>
2015-08-19 13:36:11 -0500

    Change how we define `pairs`
    Otherwise it doesn't play nice with jquery :(

--- src/ext/object.js
+++ src/ext/object.js
@@ -1,7 +1,10 @@
 if (!Object.prototype.pairs) {
-  Object.prototype.pairs = function () {
-    Object.keys(this).map(function (key) {
-      [key, this[key]];
-    }, this);
-  };
+  Object.defineProperty(Object.prototype, "pairs", {
+    value: function() {
+      return Object.keys(this).map(function (key) {
+        return [key, this[key]];
+      }, this);
+    },
+    enumerable: false
+  });
 }
