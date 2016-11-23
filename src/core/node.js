be0de3ee85fc797ad8a1fd8376957e480054b51c
jphager2 <jphager2@gmail.com>
2016-11-23 22:25:41 +0100

    check for empty string when binding value to node

--- src/core/node.js
+++ src/core/node.js
@@ -363,7 +363,7 @@ pw.node = {
     } else if (node.tagName === 'TEXTAREA' || pw.node.isSelfClosingTag(node)) {
       node.value = value;
     } else {
-      if (value) {
+      if (value || value === '') {
         node.innerHTML = value;
       }
     }
